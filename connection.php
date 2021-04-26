49c3304c04ee4170faada6c7f06591235412893e
99jais <58663686+99jais@users.noreply.github.com>
2021-04-26 18:04:37 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php 
-$servername='127.0.0.1:3307';
-$username='root';
-$password='';
-$db='meet';
+// $servername='127.0.0.1:3307';
+// $username='root';
+// $password='';
+// $db='meet';
+
+$servername='remotemysql.com';
+$username='MC3IqWykWR';
+$password='bvXSFfffJ0';
+$db='MC3IqWykWR';
 
 $connection= mysqli_connect("$servername","$username","$password","$db");
 
