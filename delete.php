4e953418515376c709f384e0cf5b8b2e1b49a3bf
99jais <58663686+99jais@users.noreply.github.com>
2021-04-26 18:11:54 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,5 +3,5 @@ include("connection.php");
     $id = $_GET['id'];
    $sql= "DELETE FROM meeting_details WHERE si_no=$id";
    $result = $connection->query($sql);
-   header('Location: Index.php');
+   header('Location: index.php');
 ?>
